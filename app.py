33f9ceb81285865c433e2d7b901e5ca06f4c5b89
Sadat Daniel <sadatdaniel@gmail.com>
2020-05-08 00:44:15 +0300

    adding API

--- app.py
+++ app.py
@@ -7,11 +7,17 @@ from predict import Predict
 from flask import Flask, render_template
 from form import chatForm
 from flask_bootstrap import Bootstrap
+from flask_restful import Resource, Api
+from check import authenticate, identity
+from flask_jwt import JWT, jwt_required
+
 
 app = Flask(__name__)
 Bootstrap(app)
 topSecret = os.urandom(50)
 app.config['SECRET_KEY'] = topSecret
+api = Api(app)
+jwt = JWT(app,authenticate,identity)
 
 a = Predict()
 
@@ -25,6 +31,22 @@ def home():
         return render_template('home.html', form=form, userInput=userInput, response=response)
     return render_template('home.html', form=form, userInput=userInput)
 
+
+class ChatAPI(Resource):
+
+  #  @jwt_required()
+  # commented out to avoid having access token. Implemented to make the application
+  # future-proof
+
+    def post(self,userInput):
+        
+        response = a.getResult(userInput,3)
+
+        return response
+        
+
+
+api.add_resource(ChatAPI, '/api/<string:userInput>')
 
 if __name__ == '__main__':
     app.run(debug=True)
